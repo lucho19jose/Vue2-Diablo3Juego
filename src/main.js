b742a2bebb65426569e82d8fc81cb6889177a6cc
lucho19jose <barbozagonzalesjose@gmail.com>
2021-09-22 08:40:37 -0500

    config, get token from blizar

--- src/main.js
+++ src/main.js
@@ -1,12 +1,30 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// Traer el css
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Darlo de alta
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  methods: {
+    init () {
+      store.dispatch('oauth/getToken', null, { root: true })
+      console.log('Hola 🌝')
+    }
+  },
+  created () {
+    this.init()
+  },
   render: h => h(App)
 }).$mount('#app')
