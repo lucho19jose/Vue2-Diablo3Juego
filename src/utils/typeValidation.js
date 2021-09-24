70477892b7b1906bc7f5d146320c833b6742df7b
lucho19jose <barbozagonzalesjose@gmail.com>
2021-09-24 13:48:34 -0500

    stats

---
+++ src/utils/typeValidation.js
@@ -0,0 +1,17 @@
+/**
+ * Used for custom validations
+ * @param hero {String}
+ * @param time {String}
+ * @param classSlug {String}
+ * @constructor
+ */
+
+function HeroData (hero, time, classSlug) {
+  this.hero = hero
+  this.time = time
+  this.classSlug = classSlug
+}
+
+export {
+  HeroData
+}
